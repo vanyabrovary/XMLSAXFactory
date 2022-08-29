b5de9b96b1e078d84827f25f71e8770845f414c8
Ivan <vanyabrovary@users.noreply.github.com>
2022-08-29 09:19:32 +0300

    Update Parse.pm

--- lib/Parse.pm
+++ lib/Parse.pm
@@ -1,19 +1,17 @@
 package Parse;
 
 use warnings; use strict;
+use Parse::Factory;
+use XML::SAX::ParserFactory;
 
 sub xml_file {
-
-    use Parse::Factory;
     my $handler = Parse::Factory->initialise('SomeFile');
     $handler->store('SomeFile');
 
-    use XML::SAX::ParserFactory;
     my $parser = XML::SAX::ParserFactory->parser( Handler => $handler );
     $parser->parse_file( 'c.xml' );
 
     return 1;
-
 }
 
 
